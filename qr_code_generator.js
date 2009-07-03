6795f231e3a401ed90214d7801fe5db840058843
Scott Ferguson <sferguson@quikorder.com>
2009-07-03 16:39:26 -0500

    Resize logo

--- qr_code_generator.js
+++ qr_code_generator.js
@@ -1,6 +1,6 @@
 jetpack.statusBar.append({
   html: <>
-    <img src="logo.png"/>
+    <img src="http://github.com/scottferg/QR-Code-Jetpack-Plugin/raw/0a0e141263c7c468efbd293a83858369203e899b/logo.png"/>
   </>,
   onReady: function(widget){
     $(widget).click(function(){
