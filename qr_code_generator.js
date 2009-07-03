5fc2e31abb61a17fb09d9b47f2a960e6dd1b282b
Scott Ferguson <sferguson@quikorder.com>
2009-07-03 17:04:57 -0500

    Fixed logo image reference

--- qr_code_generator.js
+++ qr_code_generator.js
@@ -1,7 +1,5 @@
 jetpack.statusBar.append({
-  html: <>
-    <img src="http://github.com/scottferg/QR-Code-Jetpack-Plugin/raw/0a0e141263c7c468efbd293a83858369203e899b/logo.png"/>
-  </>,
+  html: "<img src='http://www.stridesforshelter.com/content/logo.png' />",
   onReady: function(widget){
     $(widget).click(function(){
       jetpack.tabs.open( 'http://chart.apis.google.com/chart?cht=qr&chs=225x225&chl=' + escape( jetpack.tabs.focused.url ) )
