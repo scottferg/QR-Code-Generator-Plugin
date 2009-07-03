f6aeb41be1b1a32e607e8aff56be7115b5ba76f0
Scott Ferguson <sferguson@quikorder.com>
2009-07-03 15:48:20 -0500

    Removed an unnecessary <span> line

--- qr_code_generator.js
+++ qr_code_generator.js
@@ -1,7 +1,6 @@
 jetpack.statusBar.append({
   html: <>
     <img src="http://www.androidandme.com/favicon.png"/>
-      <span id="count" style="position:absolute;left:4px;top:8px;font-size:10px;cursor: pointer;background-color:rgba(255,255,255, .8);"></span>
   </>,
   onReady: function(widget){
     $(widget).click(function(){
